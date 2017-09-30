939b524578c60a24f130958578d084db48ab99d5
Yi Min Wang <yimin@Yis-MacBook-Pro.local>
2017-09-29 20:30:27 -0400

    can request random numbers and generate random bitmap

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Random
+Generate a truly Random Bitmap Image
+
+When you press the "Generate Random Image Button" we are making a request to random.org for almost 50,000 random integers ranging from 0 to 256.
+
+You might have to install the following chrome plugin if you get the "...is not allowed by Access-Control-Allow-Origin" error  https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi/related?hl=en-US
+
+Be careful, if you go over your alloted 'bits' from random.org, you won't be able to request more random numbers.
--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,41 @@
 $(document).ready(function() {
-    // all custom jQuery will go here
-    console.log('hellow world');
+    // using jQuery to hack this together
+    $('#main').click(() => {
+      getRandNums()
+        .then(nums => {
+          generateBitMap(nums);
+        });
+    });
+
     function generateBitMap(arr){
       const c = document.getElementById("myCanvas");
       const ctx = c.getContext("2d");
       const imgData = ctx.createImageData(128, 128);
 
       let i;
-      for (i = 0; i < imgData.data.length; i += 4) {
-        imgData.data[i+0] = arr[i+0];
-        imgData.data[i+1] = arr[i+0] - 255;
-        imgData.data[i+2] = arr[i+0] - 255;
-        imgData.data[i+3] = arr[i+0];
+      for (i = 0; i < imgData.data.length; i ++) {
+        imgData.data[i * 4+0] = arr[i * 3+0];
+        imgData.data[i * 4+1] = arr[i * 3+1];
+        imgData.data[i * 4+2] = arr[i * 3+2];
+        imgData.data[i * 4+3] = 256;
       }
       ctx.putImageData(imgData, 0, 0);
+    };
+
+    function getRandNums() {
+      let nums = [];
+      return new Promise(resolve => {
+        let count = 0;
+        for(let i = 0; i < 5; i++) {
+          const randUrl = 'https://random.org/integers/?num=10000&min=0&max=256&col=1&base=10&format=plain&rnd=new';
+          $.get(randUrl, data => {
+            let myNums = data.split('\n');
+            myNums.splice(myNums.length - 1, 1);
+            nums = nums.concat(myNums);
+            count++;
+            if (count === 5) resolve(nums);
+          });
+        }
+      });
     }
-    $('#main').click(() => {
-      const arr = [];
-      for (let i = 0; i < 128*128*4; i++) arr.push(255);
-      generateBitMap(arr);
-    });
 });
